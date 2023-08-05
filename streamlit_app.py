9bafa239d0650d1994258fe5e0c7bd8a83856088
nishal87 <95517478+nishal87@users.noreply.github.com>
2023-08-05 22:55:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 
-streamlit.HEADER('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.Text('Omega 3 And  Blueberry Oatmeal')
 streamlit.Text('Kale, Spinach & Rocket Smoothie')
 streamlit.Text('Hard-Boiled Free-Range Egg')
